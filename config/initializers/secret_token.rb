5e8eb479b7c93d2f72bb08529a6c4feba7eeb50f
Michael Glozman <mike_glozman@yahoo.com>
2011-01-13 22:43:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coding::Application.config.secret_token = 'e3663307d50dfaef7dc94c836eb9c9e19ace11b2907a45210d6fc032b4ab8d0ff344b123b5049769359f5d2863129048e8c14ee5c6fc68daa0dfa93937e30603'
---
+++ crap/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crap::Application.config.secret_token = '0c230e3537129a9b8b8fc3d5fe0da8d082160e8f43c84fa2d33e46a8d615ff12cec5bbea8a2be03335576652dc74452275469d6fba0c5d141f7df2445cd95602'
